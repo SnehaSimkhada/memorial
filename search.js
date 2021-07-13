0a7cdaa3acfdd41021ff9ffa54c76ae9f20d242c
Sneha4321 <71457762+Sneha4321@users.noreply.github.com>
2021-07-13 14:43:47 -0500

    Built the layout

---
+++ search.js
@@ -0,0 +1,15 @@
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("list");
+  li = div.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    txtValue = li[i].textContent || li[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li.style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
